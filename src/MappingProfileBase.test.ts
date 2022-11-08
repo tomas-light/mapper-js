ff13be7571a223efe626efe4b8a189a83bb866f6
Artem Ignatev <artem_ignatev@epam.com>
2022-11-08 13:57:52 +0100

    bugfix autoMap

--- src/MappingProfileBase.test.ts
+++ src/MappingProfileBase.test.ts
@@ -1,5 +1,82 @@
 import { MappingProfileBase } from './MappingProfileBase';
 
+test('if it is a new object', () => {
+	const source = { some: 'qqq' };
+	const destination = MappingProfileBase.autoMap(source, {} as typeof source);
+	expect(destination).not.toBe(source);
+});
+
+describe('if primitive properties are mapped', () => {
+	const symbol = Symbol('any custom symbol');
+	const source = {
+		stringProperty: 'qqq',
+		numberProperty: 123,
+		booleanProperty: true,
+		undefinedProperty: undefined,
+		symbolProperty: symbol,
+	};
+	const destination = MappingProfileBase.autoMap(source, {} as typeof source);
+
+	test('string property is mapped', () => {
+		expect(destination.stringProperty).toBe('qqq');
+	});
+	test('number property is mapped', () => {
+		expect(destination.numberProperty).toBe(123);
+	});
+	test('boolean property is mapped', () => {
+		expect(destination.booleanProperty).toBe(true);
+	});
+	test('undefined property is mapped', () => {
+		expect('undefinedProperty' in destination).toBeTruthy();
+		expect(destination.undefinedProperty).toBe(undefined);
+	});
+	test('symbol property is mapped', () => {
+		expect(destination.symbolProperty).toBe(symbol);
+	});
+});
+
+describe('if object properties are mapped', () => {
+	const array = ['item 1', 'item 2', 'item 3'];
+	const source = {
+		nullProperty: null,
+		arrayProperty: array,
+	};
+	const destination = MappingProfileBase.autoMap(source, {} as typeof source);
+
+	test('null property is mapped', () => {
+		expect(destination.nullProperty).toBe(null);
+	});
+	test('array property is mapped', () => {
+		expect(destination.arrayProperty).toEqual(['item 1', 'item 2', 'item 3']);
+	});
+	test('array property is mapped to new array', () => {
+		expect(destination.arrayProperty).not.toBe(array);
+	});
+
+	describe('nested objects', () => {
+		const source = {
+			some: {
+				stringProperty: 'qqq',
+			},
+			another: {
+				numberProperty: 444,
+			},
+		};
+
+		const destination = MappingProfileBase.autoMap(source, {} as typeof source);
+
+		test('nested objects has different refs', () => {
+			expect(destination.some).not.toBe(source.some);
+			expect(destination.another).not.toBe(source.another);
+		});
+
+		test('nested objects are fully copied', () => {
+			expect(destination.some.stringProperty).toBe(source.some.stringProperty);
+			expect(destination.another.numberProperty).toBe(source.another.numberProperty);
+		});
+	});
+});
+
 test('filled destination', () => {
 	const obj1 = {
 		prop1: 'qqq',
@@ -20,44 +97,10 @@ test('filled destination', () => {
 test('empty destination', () => {
 	const obj1 = {
 		prop1: 'qqq',
-		prop2: 123,
 	};
 	const obj2 = {};
 
 	const result = MappingProfileBase.autoMap(obj1, obj2);
 	expect(result).toBe(obj2);
 	expect(obj2['prop1']).toBe('qqq');
-	expect(obj2['prop2']).toBe(123);
-});
-
-describe('nested objects', () => {
-	const obj1 = {
-		prop1: {
-			prop11: 'qqq',
-			prop12: 123,
-		},
-		prop2: {
-			prop21: 444,
-			prop22: '345',
-		},
-	};
-	const obj2: typeof obj1 = {} as any;
-
-	const result = MappingProfileBase.autoMap(obj1, obj2);
-
-	test('mapper doesnt create new object for destination', () => {
-		expect(result).toBe(obj2);
-	});
-
-	test('nested objects has different refs', () => {
-		expect(obj2.prop1).not.toBe(obj1.prop1);
-		expect(obj2.prop2).not.toBe(obj1.prop2);
-	});
-
-	test('nested objects are fully copied', () => {
-		expect(obj2.prop1.prop11).toBe(obj1.prop1.prop11);
-		expect(obj2.prop1.prop12).toBe(obj1.prop1.prop12);
-		expect(obj2.prop2.prop21).toBe(obj1.prop2.prop21);
-		expect(obj2.prop2.prop22).toBe(obj1.prop2.prop22);
-	});
 });
--- src/MappingProfileBase.ts
+++ src/MappingProfileBase.ts
@@ -1,17 +1,32 @@
 export class MappingProfileBase {
-	static autoMap<TSource extends object, TDestination extends object>(
-		sourceModel: TSource,
-		destinationModel: TDestination
-	): TDestination {
+	static autoMap<Source extends object, Destination extends object = Source>(
+		sourceModel: Source,
+		destinationModel: Destination
+	): Destination {
+		if (typeof sourceModel !== 'object') {
+			return sourceModel;
+		}
+
+		if (sourceModel === null) {
+			return null as any;
+		}
+
+		if (Array.isArray(sourceModel)) {
+			return sourceModel.map((element) => MappingProfileBase.autoMap(element, {})) as Destination;
+		}
+
 		let keys = Object.keys(destinationModel);
 		if (keys.length === 0) {
 			keys = Object.keys(sourceModel);
 		}
 
-		keys.forEach((propertyName: string) => {
+		keys.forEach((propertyName) => {
 			if (propertyName in sourceModel) {
 				if (typeof sourceModel[propertyName] === 'object') {
-					destinationModel[propertyName] = MappingProfileBase.autoMap(sourceModel[propertyName], {});
+					destinationModel[propertyName] = MappingProfileBase.autoMap(
+						sourceModel[propertyName],
+						destinationModel[propertyName] ?? {}
+					);
 				} else {
 					destinationModel[propertyName] = sourceModel[propertyName];
 				}
